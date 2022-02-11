8e964afc08f243a0b536c2430ffb86e491185cfe
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-11 14:24:52 -0500

    logic is working, need to style it a bit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @query = params[:question]
+   if @query == 'I am going to work' 
+    @answer = 'Great!'
+   elsif @query.include?("?")
+    @answer = "Silly question, get dressed and go to work!"
+   else 
+    @answer = "I don't care, get dressed and go to work!"
+   end
   end
 end
